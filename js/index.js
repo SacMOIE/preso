62d3fffe95ad98161f4d457ecaea67704652eaf5
Abhi Nemani <abhi.nemani@gmail.com>
2016-10-23 08:28:32 -0500

    tweaked font sizes

--- js/index.js
+++ js/index.js
@@ -18,10 +18,10 @@ var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1mxrXQbNi9d
 			for (var i=0; i<data.length; i++) {
 				str += "<section  class='future' data-background-image='http://abhinemani.com/preso/img/"+data[i].bg+"'>";
 				str += "<h1>" + data[i].Organization + "</h1>";
-				str += "<h2>" + data[i].Applicant + "</h2>";
+				str += "<h3>" + data[i].Applicant + "</h3>";
 				str += "<p>" + data[i].Description + "</p>";
 				
-				str += "<h3>Proposed Grant: " + data[i].Grant + "</h3>";
+				str += "<h3>" + data[i].Grant + "</h3>";
 				str += "</section>";				
 			}
 			gsheetClass.outerHTML=str; // it's simple replacement of whole element with contents of str var
