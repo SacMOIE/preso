562b86f62a147e31fa832e4db98509eef1afe2ea
Abhi Nemani <abhi.nemani@gmail.com>
2016-10-23 15:06:38 -0500

    added more backgrounds

--- js/index.js
+++ js/index.js
@@ -17,12 +17,10 @@ var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1mxrXQbNi9d
 		if(gsheetClass.outerHTML) { // if outerHTML is supported
 			for (var i=0; i<data.length; i++) {
 				str += "<section  class='future' data-background-image='http://abhinemani.com/preso/img/"+data[i].bg+"'>";
-				str += "<h4> // " + data[i].Category + "</h4>";
-				str += "<h1>" + data[i].Organization + "</h1>";
+				str += "<h1> <a target='_blank' href='" + data[i].Link + "'>" + data[i].Organization + "</a></h1>";
+				str += "<h4>" + data[i].Category + "  // " + data[i].Applicant + "   <a target='_blank' href='mailto:" + data[i].Email + "'><i class='fa fa-envelope-o' aria-hidden='true'></i></a> // "+  data[i].Grant + "</h4>";
 				str += "<blockquote>" + data[i].Quote + "</blockquote>";
-				str += "<p>" + data[i].Description + "</p>";
-				str += "<h3>" + data[i].Applicant + " | " + data[i].Grant + "</h3>";
-				
+				str += "<p><strong>" + data[i].Description + "</strong></p>";
 				str += "</section>";				
 			}
 			gsheetClass.outerHTML=str; // it's simple replacement of whole element with contents of str var
