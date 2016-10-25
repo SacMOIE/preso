1c46a95e365629a80e28c47855dfe28cabfe5b95
Abhi Nemani <abhi.nemani@gmail.com>
2016-10-24 22:42:18 -0500

    added permalinks

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1mxrXQbNi9d
 		var gsheetClass = document.getElementsByClassName(target_gsheet_class_once)[0];
 		if(gsheetClass.outerHTML) { // if outerHTML is supported
 			for (var i=0; i<data.length; i++) {
-				str += "<section  class='future' data-background-image='img/"+data[i].bg+"' id='" + data[i].Organization + "'>";
+				str += "<section  class='future' data-background-image='img/"+data[i].bg+"'>";
 				str += "<h1> <a target='_blank' href='" + data[i].Link + "'>" + data[i].Organization + "</a></h1>";
 				str += "<h4>" + data[i].Category + "  // " + data[i].Applicant + "   <a target='_blank' href='mailto:" + data[i].Email + "'><i class='fa fa-envelope-o' aria-hidden='true'></i></a> // "+  data[i].Grant + "</h4>";
 				str += "<blockquote>" + data[i].Quote + "</blockquote>";
